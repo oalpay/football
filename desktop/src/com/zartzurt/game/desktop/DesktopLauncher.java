7033be7194314459141b2cfde942d51f1f95d26e
Osman A <osman.alpay@gmail.com>
2014-04-23 22:59:05 +0300

    init

---
+++ desktop/src/com/zartzurt/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.zartzurt.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.zartzurt.game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
