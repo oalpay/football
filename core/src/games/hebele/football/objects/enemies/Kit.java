49542eaa52fc43bc51061c282cb1c5e5af25448e
Osman A <osman.alpay@gmail.com>
2014-05-13 22:22:46 +0300

    converted to utf8

--- core/src/games/hebele/football/objects/enemies/Kit.java
+++ core/src/games/hebele/football/objects/enemies/Kit.java
@@ -1,28 +1,27 @@
 package games.hebele.football.objects.enemies;
 
-import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.Animation.PlayMode;
+import games.hebele.football.objects.Player;
+
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import games.hebele.football.objects.Player;
 
 public class Kit extends Enemy {
 
 	public Kit(World world, Stage stage, Player player, float posX, float posY) {
 		super(world, stage, player, posX, posY);
-		
-		//SET ENEMY ANIMATION (FROM TEXTURE ATLAS)
+
+		// SET ENEMY ANIMATION (FROM TEXTURE ATLAS)
 		setEnemyAnimation("kit");
-		
-		//CHANGES THE IMAGE SIZE (NOT THE BOX2D OBJECT)
-		setSize(getWidth(), getHeight()*1.3f);
-		
-		//SET THE POSSIBLE SPEECH OF ENEMY
-		textAlternatives.add("KÝT");
-		textAlternatives.add("Oðlum oynamasanýza burada!");
+
+		// CHANGES THE IMAGE SIZE (NOT THE BOX2D OBJECT)
+		setSize(getWidth(), getHeight() * 1.3f);
+
+		// SET THE POSSIBLE SPEECH OF ENEMY
+		textAlternatives.add("Kï¿½T");
+		textAlternatives.add("Oï¿½lum oynamasanï¿½za burada!");
 		textAlternatives.add("Keserim topunu!");
-		
-		//SET THE ENEMY MOVEMENT BEHAVIOUR
+
+		// SET THE ENEMY MOVEMENT BEHAVIOUR
 		setBehaviourMovement(new MovementStand(this));
 	}
 
